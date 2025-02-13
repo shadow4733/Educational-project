d7037d4c28d3c6c5e53d209806248209d968ad9e
Bogdan <bogdan.rubanik28@gmail.com>
2025-02-13 20:58:27 +0700

    [Initial] Project structure and main menu [implementation]

---
+++ src/main.py
@@ -0,0 +1,33 @@
+import pygame
+from utils.constants import SCREEN_WIDTH, SCREEN_HEIGHT, FPS, WHITE, BLACK, RED, GREEN, BLUE
+from ui.menus import MainMenu
+
+def main():
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Space Roguelike")
+
+    menu = MainMenu(screen)
+
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            menu.handle_event(event)
+
+        screen.fill(BLACK)
+
+        menu.draw()
+
+        pygame.display.flip()
+
+        clock.tick(FPS)
+
+    pygame.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ src/utils/constants.py
@@ -0,0 +1,13 @@
+# Размеры экрана
+SCREEN_WIDTH = 1550
+SCREEN_HEIGHT = 800
+
+# Цвета
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# FPS
+FPS = 60
