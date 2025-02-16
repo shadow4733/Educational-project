a22bb31ec846363a358d6cb150316fd82637ee27
Andrey Zhdanov <CayMan.1@bk.ru>
2025-02-16 23:09:01 +0700

    добавил другие звуки, более качественные, обрезал старые, чтобы не мусорить звуком

--- game1/sounds_game.py
+++ game1/sounds_game.py
@@ -1,14 +1,14 @@
 import pygame
 def bg_music():
-    pygame.mixer.music.load("sounds/BACKGROUND.mp3")  # музыка на фон
+    pygame.mixer.music.load("sounds/CRAZYFROG.mp3")  # музыка на фон
     pygame.mixer.music.play(-1, 0.0, 500)
     pygame.mixer.music.set_volume(0.05)
 def jump():
-    jumping = pygame.mixer.Sound("sounds/JUMP.ogg")
+    jumping = pygame.mixer.Sound("sounds/JUMPNEW.MP3")
     jumping.play()
     jumping.set_volume(0.6)
 def hit():
-    hitting = pygame.mixer.Sound("sounds/HIT.ogg")
+    hitting = pygame.mixer.Sound("sounds/HITNEW.MP3")
     hitting.play()
 def run():
     pygame.mixer.music.load("sounds/RUN.ogg")
