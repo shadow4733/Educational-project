64073fdfc17d659bd18a87f7eef5dabc302875b7
Bogdan <bogdan.rubanik28@gmail.com>
2025-02-21 22:35:32 +0700

    [added] new colors

--- game1/constant/constnants.py
+++ game1/constant/constnants.py
@@ -5,6 +5,7 @@ WIDTH = 1600
 HEIGHT = 1080
 HEALTH = 100
 SCORE = 0
+SCORE_TIMER = 0
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # ФПС
@@ -15,6 +16,9 @@ WHITE = (255, 255, 255)
 GRAY = (170, 170, 170)
 BLACK = (0, 0, 0)
 GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+PINK = (255,102,102)
+LIGHT_PINK = (255,204,204)
 
 # Герой
 HERO_COLOR = (0, 255, 0)
