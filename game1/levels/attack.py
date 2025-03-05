e804b551d787a3d084f0af52bf5327c9c315e4cb
Bogdan <bogdan.rubanik28@gmail.com>
2025-03-06 01:22:38 +0700

    [feature] added new attack

--- game1/levels/attack.py
+++ game1/levels/attack.py
@@ -3,9 +3,17 @@ import pygame
 from game1.constant.constnants import HEIGHT, WIDTH
 
 ATTACK_DAMAGE = {
-    "sword_vertical": 1,
-    "sword_horizontal_left": 1,
-    "dragon_vertical": 5,
+    "sword1_vertical": 1,
+    "sword1_horizontal_left": 1,
+    "dragon_vertical": 1,
+    "dragon_horizontal": 1,
+    "chicken_vertical": 1,
+    "chicken_horizontal": 1,
+    "sword2_vertical": 1,
+    "sword2_horizontal_left": 1,
+    "fireball_vertical": 1,
+    "fireball_horizontal": 1,
+
 }
 
 def get_attack_damage(attack_type):
@@ -13,7 +21,7 @@ def get_attack_damage(attack_type):
     return ATTACK_DAMAGE.get(attack_type, 0)
 
 # Спрайт для вертикально летящих мечей
-class sword_vertical(pygame.sprite.Sprite):
+class sword1_vertical(pygame.sprite.Sprite):
     def __init__(self, image):
         super().__init__()
         self.image = image  # Используем переданное изображение
@@ -33,25 +41,27 @@ class sword_vertical(pygame.sprite.Sprite):
             self.speedy = 1
 
 # Спрайт для горизонтально летящих мечей
-class sword_horizontal(pygame.sprite.Sprite):
+class sword1_horizontal(pygame.sprite.Sprite):
     def __init__(self, image):
         super().__init__()
         self.image = image  # Используем переданное изображение
         self.rect = self.image.get_rect()
         self.rect.x = random.randrange(-200, -70)
         self.rect.y = random.randrange(500, 1000)
-        self.speedx = 12
+        self.speedx = 12  # Скорость движения по оси x
 
     def reset_position(self):
         self.rect.x = random.randrange(-200, -70)
         self.rect.y = random.randrange(500, 1000)
 
     def update(self):
+        # Обновляем позицию объекта, двигаясь по оси x
         self.rect.x += self.speedx
+        # Если объект выходит за пределы экрана, сбрасываем его на новое место
         if self.rect.left > WIDTH + 80:
             self.reset_position()
-            self.speedx = 1
 
+# Спрайт для вертикально летящих драконов
 class dragon_vertical(pygame.sprite.Sprite):
     def __init__(self, image):
         super().__init__()
@@ -70,3 +80,147 @@ class dragon_vertical(pygame.sprite.Sprite):
         if self.rect.top > HEIGHT + 50:
             self.reset_position()
             self.speedy = 1
+
+# Спрайт для горизонтально летящих драконов
+class dragon_horizontal(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = image  # Используем переданное изображение
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(-200, -70)
+        self.rect.y = random.randrange(500, 1000)
+        self.speedx = 12  # Скорость движения по оси x
+
+    def reset_position(self):
+        self.rect.x = random.randrange(-200, -70)
+        self.rect.y = random.randrange(500, 1000)
+
+    def update(self):
+        # Обновляем позицию объекта, двигаясь по оси x
+        self.rect.x += self.speedx
+        # Если объект выходит за пределы экрана, сбрасываем его на новое место
+        if self.rect.left > WIDTH + 80:
+            self.reset_position()
+
+# Спрайт для вертикально летящей курицы
+class chicken_vertical(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = image  # Используем переданное изображение
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(700, 1200)
+        self.rect.y = random.randrange(-200, -70)
+        self.speedy = 12
+
+    def reset_position(self):
+        self.rect.x = random.randrange(500, 1400)
+        self.rect.y = random.randrange(-200, -70)
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 50:
+            self.reset_position()
+            self.speedy = 1
+
+# Спрайт для горизонтально летящей курицы
+class chicken_horizontal(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = image  # Используем переданное изображение
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(-200, -70)
+        self.rect.y = random.randrange(500, 1000)
+        self.speedx = 12  # Скорость движения по оси x
+
+    def reset_position(self):
+        self.rect.x = random.randrange(-200, -70)
+        self.rect.y = random.randrange(500, 1000)
+
+    def update(self):
+        # Обновляем позицию объекта, двигаясь по оси x
+        self.rect.x += self.speedx
+        # Если объект выходит за пределы экрана, сбрасываем его на новое место
+        if self.rect.left > WIDTH + 80:
+            self.reset_position()
+
+# Спрайт для вертикально летящих мечей2
+class sword2_vertical(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = image  # Используем переданное изображение
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(700, 1200)
+        self.rect.y = random.randrange(-200, -70)
+        self.speedy = 12
+
+    def reset_position(self):
+        self.rect.x = random.randrange(500, 1400)
+        self.rect.y = random.randrange(-200, -70)
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 50:
+            self.reset_position()
+            self.speedy = 1
+
+# Спрайт для горизонтально летящих мечей2
+class sword2_horizontal(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = image  # Используем переданное изображение
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(-200, -70)
+        self.rect.y = random.randrange(500, 1000)
+        self.speedx = 12  # Скорость движения по оси x
+
+    def reset_position(self):
+        self.rect.x = random.randrange(-200, -70)
+        self.rect.y = random.randrange(500, 1000)
+
+    def update(self):
+        # Обновляем позицию объекта, двигаясь по оси x
+        self.rect.x += self.speedx
+        # Если объект выходит за пределы экрана, сбрасываем его на новое место
+        if self.rect.left > WIDTH + 80:
+            self.reset_position()
+
+# Спрайт для вертикально летящего фаербола
+class fireball_vertical(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = image  # Используем переданное изображение
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(700, 1200)
+        self.rect.y = random.randrange(-200, -70)
+        self.speedy = 12
+
+    def reset_position(self):
+        self.rect.x = random.randrange(500, 1400)
+        self.rect.y = random.randrange(-200, -70)
+
+    def update(self):
+        self.rect.y += self.speedy
+        if self.rect.top > HEIGHT + 50:
+            self.reset_position()
+            self.speedy = 1
+
+# Спрайт для горизонтально летящего фаербола
+class fireball_horizontal(pygame.sprite.Sprite):
+    def __init__(self, image):
+        super().__init__()
+        self.image = image  # Используем переданное изображение
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(-200, -70)
+        self.rect.y = random.randrange(500, 1000)
+        self.speedx = 12  # Скорость движения по оси x
+
+    def reset_position(self):
+        self.rect.x = random.randrange(-200, -70)
+        self.rect.y = random.randrange(500, 1000)
+
+    def update(self):
+        # Обновляем позицию объекта, двигаясь по оси x
+        self.rect.x += self.speedx
+        # Если объект выходит за пределы экрана, сбрасываем его на новое место
+        if self.rect.left > WIDTH + 80:
+            self.reset_position()
