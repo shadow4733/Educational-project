6454dc6a50413993e2c19358ad3028982dac7e22
Andrey Zhdanov <CayMan.1@bk.ru>
2025-03-07 10:19:41 +0700

    Добавление новых ивентов в уровень

--- game1/levels/events/event_level_3.py
+++ game1/levels/events/event_level_3.py
@@ -16,19 +16,23 @@ WHITE_RECT_HEIGHT = 500
 events = [
     (1, 1, (WHITE_RECT_X + WHITE_RECT_WIDTH, WHITE_RECT_Y), "sword1_diagonal2"),
     (1, 1, (WHITE_RECT_X, WHITE_RECT_Y), "sword1_diagonal"),
+
     (2, 1, (WIDTH // 2, HEIGHT // 2 - 300), "sword1_vertical"),
     (2, 1, (WIDTH // 2 + 50, HEIGHT // 2 - 300), "sword1_vertical"),
     (2, 1, (WIDTH // 2 + 100, HEIGHT // 2 - 300), "sword1_vertical"),
     (2, 1, (WIDTH // 2 + 150, HEIGHT // 2 - 300), "sword1_vertical"),
     (2, 1, (WIDTH // 2 + 200, HEIGHT // 2 - 300), "sword1_vertical"),
+
     (3, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 350), "sword1_horizontal_left"),
     (3, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 400), "sword1_horizontal_left"),
+
     (5, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 100), "sword1_horizontal_left"),
     (5, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 100), "sword1_horizontal_left"),
     (5, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 100), "sword1_horizontal_left"),
     (5, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 150), "sword1_horizontal_left"),
     (5, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 200), "sword1_horizontal_left"),
     (5, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 250), "sword1_horizontal_left"),
+
     (6, 1, (WIDTH // 2 - 50, HEIGHT // 2 - 300), "sword1_vertical"),
     (6, 1, (WIDTH // 2, HEIGHT // 2 - 300), "sword1_vertical"),
     (6, 1, (WIDTH // 2 + 50, HEIGHT // 2 - 300), "sword1_vertical"),
@@ -36,12 +40,23 @@ events = [
     (6, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 200), "sword1_horizontal_left"),
     (6, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 250), "sword1_horizontal_left"),
     (6, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 300), "sword1_horizontal_left"),
-    (7, 1, (WIDTH // 2 - 50, HEIGHT // 2 - 300), "dragon_vertical"),
-    (7, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 300), "dragon_horizontal"),
+
+    (7, 1, (WIDTH // 2 - 100, HEIGHT // 2 - 200), "dragon_vertical"),
+    (7, 1, (WIDTH // 2 - 400, HEIGHT // 2 + 200), "dragon_horizontal"),
+
     (8, 1, (WIDTH // 2 - 50, HEIGHT // 2 - 300), "chicken_vertical"),
     (8, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 300), "chicken_horizontal"),
-    (9, 1, (WIDTH // 2 - 50, HEIGHT // 2 - 300), "sword2_vertical"),
-    (9, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 300), "sword2_horizontal_left"),
-    (10, 1, (WIDTH // 2 - 50, HEIGHT // 2 - 300), "fireball_vertical"),
-    (10, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 300), "fireball_horizontal")
+
+    (9, 1, (WHITE_RECT_X + WHITE_RECT_WIDTH, WHITE_RECT_Y), "sword1_diagonal2"),
+    (9, 1, (WHITE_RECT_X, WHITE_RECT_Y), "sword1_diagonal"),
+
+    (10, 1, (WIDTH // 2-200, HEIGHT // 2 - 250), "dragon_vertical"),
+    (10, 1, (WIDTH // 2, HEIGHT // 2 - 250), "dragon_vertical"),
+    (10, 1, (WIDTH // 2+200, HEIGHT // 2 - 250), "dragon_vertical"),
+    (10, 1, (WIDTH // 2-300, HEIGHT // 2+50), "dragon_horizontal"),
+    (10, 1, (WIDTH // 2-300, HEIGHT // 2+250), "dragon_horizontal"),
+    (10, 1, (WIDTH // 2-300, HEIGHT // 2+450), "dragon_horizontal"),
+
+    (13, 1, (WIDTH // 2 - 50, HEIGHT // 2 - 300), "fireball_vertical"),
+    (13, 1, (WIDTH // 2 - 500, HEIGHT // 2 + 300), "fireball_horizontal")
 ]
