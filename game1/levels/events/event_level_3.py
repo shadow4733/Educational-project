1f0f3cc53a94653f643efd313904bb3dd5d7a35d
Andrey Zhdanov <CayMan.1@bk.ru>
2025-03-11 10:37:31 +0700

    В ивент прописал появление диагональных атак

--- game1/levels/events/event_level_3.py
+++ game1/levels/events/event_level_3.py
@@ -14,8 +14,21 @@ WHITE_RECT_HEIGHT = 500
 
 
 events = [
-    (1, 1, (WHITE_RECT_X + WHITE_RECT_WIDTH, WHITE_RECT_Y), "sword1_diagonal2"),
-    (1, 1, (WHITE_RECT_X, WHITE_RECT_Y), "sword1_diagonal"),
+    (0.5, 1, (WHITE_RECT_X + WHITE_RECT_WIDTH, WHITE_RECT_Y), "sword2_diagonal2"),
+    (0.5, 1, (WHITE_RECT_X, WHITE_RECT_Y), "sword2_diagonal"),
+
+    (1, 1, (WHITE_RECT_X + WHITE_RECT_WIDTH, WHITE_RECT_Y), "dragon1_diagonal2"),
+    (1, 1, (WHITE_RECT_X, WHITE_RECT_Y), "dragon1_diagonal"),
+
+    (1.5, 1, (WHITE_RECT_X + WHITE_RECT_WIDTH, WHITE_RECT_Y), "chicken_diagonal2"),
+    (1.5, 1, (WHITE_RECT_X, WHITE_RECT_Y), "chicken_diagonal"),
+
+    (1.8, 1, (WHITE_RECT_X + WHITE_RECT_WIDTH, WHITE_RECT_Y), "fireball_diagonal2"),
+    (1.8, 1, (WHITE_RECT_X, WHITE_RECT_Y), "fireball_diagonal"),
+
+    (1.9, 1, (WHITE_RECT_X + WHITE_RECT_WIDTH, WHITE_RECT_Y), "sword1_diagonal2"),
+    (1.9, 1, (WHITE_RECT_X, WHITE_RECT_Y), "sword1_diagonal"),
+
 
     (2, 1, (WIDTH // 2, HEIGHT // 2 - 300), "sword1_vertical"),
     (2, 1, (WIDTH // 2 + 50, HEIGHT // 2 - 300), "sword1_vertical"),
