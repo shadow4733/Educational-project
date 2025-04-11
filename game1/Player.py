fd32a41458aeb0084f9eb51441b00b7d4be78374
Andrey Zhdanov <CayMan.1@bk.ru>
2025-04-11 13:03:00 +0700

    Добавил картинку для персонажа в класс Player

--- game1/Player.py
+++ game1/Player.py
@@ -1,13 +1,15 @@
 # Спрайт игрока
 import pygame
-
+from os import path
 from game1.constant.constnants import HERO_COLOR, WIDTH, HEIGHT, HEALTH, HERO_SIZE
+
+player_img = pygame.image.load(path.join("../images/hero/herosleva.png")).convert()
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((HERO_SIZE, HERO_SIZE))
-        self.image.fill(HERO_COLOR)
+        self.image = player_img
+        #self.image.fill(HERO_COLOR)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH // 2 - HERO_SIZE // 2
         self.rect.centery = HEIGHT // 2 + 250 - HERO_SIZE // 2
