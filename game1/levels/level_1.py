7a358af33110ad754561c874f266114499856e52
Bogdan <bogdan.rubanik28@gmail.com>
2025-04-20 22:00:26 +0700

    refactor level

--- game1/levels/level_1.py
+++ game1/levels/level_1.py
@@ -1,29 +1,93 @@
 import pygame
 import sys
-
+import subprocess
+import os
+from os import path
+from pyvidplayer import Video
 from game1.Player import Player
 from game1.constant.constnants import *
-from game1.levels.attack import sword1_vertical, sword1_horizontal_left, dragon_vertical, get_attack_damage, \
-    dragon_horizontal, chicken_vertical, chicken_horizontal, fireball_vertical, fireball_horizontal, \
-    sword2_vertical, sword2_horizontal_left, sword1_diagonal, sword1_diagonal2, dragon1_diagonal2, \
-    dragon1_diagonal, chicken_diagonal2, chicken_diagonal, sword2_diagonal, sword2_diagonal2, fireball_diagonal2, fireball_diagonal, \
-    fireball_horizontal_right, sword1_horizontal_right, sword2_horizontal_right, chicken_horizontal_right, dragon_horizontal_right
-from game1.levels.events.event_level_3 import events
+from game1.levels.attack import sword1_vertical, sword1_horizontal_left, get_attack_damage, chicken_vertical, \
+    chicken_horizontal, chicken_diagonal2, chicken_diagonal, sword1_horizontal_right, chicken_horizontal_right, \
+    bubble_vertical, sword1_diagonal, sword1_diagonal2, dragon_vertical, dragon_horizontal, dragon_horizontal_right, \
+    dragon1_diagonal, dragon1_diagonal2, sword2_vertical, sword2_horizontal_left, sword2_horizontal_right, \
+    sword2_diagonal, sword2_diagonal2, fireball_vertical, fireball_horizontal, fireball_horizontal_right, \
+    fireball_diagonal, fireball_diagonal2
+from game1.levels.events.event_level_1 import events
+
 
 pygame.init()
 
+current_level = "level_1.py"
+player_health_on_death = 0
+player_score_on_death = 0
+
+def display_game_over_screen(screen, font):
+    """Отображает экран 'Игра окончена' с опциями."""
+    game_over_image = pygame.image.load("../images/bg/game_over.png")
+    game_over_rect = game_over_image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
+
+    restart_button_rect = pygame.Rect(WIDTH // 2 - 150, HEIGHT // 2 + 50, 300, 50)
+    main_menu_button_rect = pygame.Rect(WIDTH // 2 - 150, HEIGHT // 2 + 120, 300, 50)
+    restart_button_color = WHITE
+    main_menu_button_color = WHITE
+    mouse_pos = pygame.mouse.get_pos()
+
+    if restart_button_rect.collidepoint(mouse_pos):
+        restart_button_color = (200, 200, 200)
+    if main_menu_button_rect.collidepoint(mouse_pos):
+        main_menu_button_color = (200, 200, 200)
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if restart_button_rect.collidepoint(event.pos):
+                    print("Выйти")
+                    pygame.quit()
+                    sys.exit()
+                elif main_menu_button_rect.collidepoint(event.pos):
+                    print("Главное меню")
+                    try:
+                        subprocess.Popen(["python", "../menu/main_menu.py"])
+                    except FileNotFoundError:
+                        print("Ошибка: main_menu.py не найден.")
+
+                    pygame.quit()
+                    sys.exit()
+
+                    running = False
+                    break
+
+        # Отрисовка
+        screen.fill(BLACK)
+        screen.blit(game_over_image, game_over_rect)
+
+        pygame.draw.rect(screen, restart_button_color, restart_button_rect)
+        pygame.draw.rect(screen, main_menu_button_color, main_menu_button_rect)
+
+        restart_text = font.render("Выйти", True, BLACK)
+        main_menu_text = font.render("Главное меню", True, BLACK)
+
+        restart_text_rect = restart_text.get_rect(center=restart_button_rect.center)
+        main_menu_text_rect = main_menu_text.get_rect(center=main_menu_button_rect.center)
+
+        screen.blit(restart_text, restart_text_rect)
+        screen.blit(main_menu_text, main_menu_text_rect)
+
+        pygame.display.flip()
+
 def start_level():
     """Уровень 1"""
-    pygame.mixer.music.load('sounds/MM 8 bit 2.mp3')
-    pygame.mixer.music.play(-1)
-    pygame.mixer.music.set_volume(0.2)
-
+    pygame.display.set_caption("Уровень 1")
     font = pygame.font.Font(None, 36)
 
     SCREEN.fill(BLACK)
 
     # Отображение текста
-    level_text = font.render("Уровень 3", True, WHITE)
+    level_text = font.render("Уровень 1", True, WHITE)
     level_text_rect = level_text.get_rect(center=(WIDTH // 2, HEIGHT - 950))
 
     # Переменные для здоровья и очков
@@ -35,6 +99,8 @@ def start_level():
     player = Player()
     play_sprites = pygame.sprite.Group()
     play_sprites.add(player)
+    player_images_right = pygame.image.load("../images/hero/herosprava.png")
+    player_images_left = pygame.image.load("../images/hero/herosleva.png")
 
     # Группа спрайтов-проджектайлов
     projectiles = pygame.sprite.Group()
@@ -46,9 +112,14 @@ def start_level():
 
     pygame.display.flip()
 
+    pygame.mixer.music.load('sounds/MM 8 bit 2.mp3')
+    pygame.mixer.music.play(-1)
+    pygame.mixer.music.set_volume(0.2)
+
     # Игровой цикл
     waiting = True
     while waiting:
+        #SCREEN.blit(background, (0, 0))  # Устанавливаем фон
 
         current_time = (pygame.time.get_ticks() - start_time) / 1000  # Время в секундах
 
@@ -62,8 +133,10 @@ def start_level():
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
+            player.image = player_images_left
             player.rect.centerx -= 5
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
+            player.image = player_images_right
             player.rect.centerx += 5
         if keys[pygame.K_UP] or keys[pygame.K_w]:
             player.rect.centery -= 5
@@ -80,7 +153,7 @@ def start_level():
         elif player.rect.centery + HERO_SIZE // 2 > HEIGHT // 2 + 495:
             player.rect.centery = HEIGHT // 2 + 495 - HERO_SIZE // 2
 
-        #Загружаем изображения проджектайлов
+        # Загружаем изображения проджектайлов
         sword1_image = pygame.image.load("../images/projectiles/sword1.png")
         dragon_image = pygame.image.load("../images/projectiles/dragon_main.png")
         chicken_image = pygame.image.load("../images/projectiles/chicken.png")
@@ -109,6 +182,7 @@ def start_level():
                         rotated_sword1 = pygame.transform.rotate(sword1_image.copy(), 90)
                         projectile_temp = sword1_horizontal_right(rotated_sword1)
 
+
                     elif direction == "dragon_vertical":
                         rotated_dragon = pygame.transform.rotate(dragon_image.copy(), 180)
                         projectile_temp = dragon_vertical(rotated_dragon)
@@ -124,6 +198,8 @@ def start_level():
                     elif direction == "dragon1_diagonal2":
                         rotated_dragon = pygame.transform.rotate(dragon_image.copy(), 135)
                         projectile_temp = dragon1_diagonal2(rotated_dragon)
+
+
 
                     elif direction == "chicken_vertical":
                         rotated_chicken = pygame.transform.rotate(chicken_image.copy(), 180)
@@ -141,11 +217,13 @@ def start_level():
                         rotated_chicken = pygame.transform.rotate(chicken_image.copy(), 135)
                         projectile_temp = chicken_diagonal2(rotated_chicken)
 
+
+
                     elif direction == "sword2_vertical":
                         rotated_sword2 = pygame.transform.rotate(sword2_image.copy(), 180)
                         projectile_temp = sword2_vertical(rotated_sword2)
                     elif direction == "sword2_horizontal_left":
-                        rotated_sword2 = pygame.transform.rotate(sword2_image.copy(), 270)
+                        rotated_sword2 = pygame.transform.rotate(sword2_image.copy(), 315)
                         projectile_temp = sword2_horizontal_left(rotated_sword2)
                     elif direction == "sword2_horizontal_right":
                         rotated_sword2 = pygame.transform.rotate(sword2_image.copy(), 135)
@@ -156,6 +234,7 @@ def start_level():
                     elif direction == "sword2_diagonal2":
                         rotated_sword2 = pygame.transform.rotate(sword2_image.copy(), 180)
                         projectile_temp = sword2_diagonal2(rotated_sword2)
+
 
                     elif direction == "fireball_vertical":
                         rotated_fireball = pygame.transform.rotate(fireball_image.copy(), 180)
@@ -211,17 +290,36 @@ def start_level():
 
         projectiles.update()
         projectiles.draw(SCREEN)
-
+        running = True
         # Проверка коллизий
         for projectile in projectiles:
-            if projectile.rect.colliderect(player):
+            if projectile.rect.colliderect(player.rect):
                 attack_type = projectile.__class__.__name__.lower()
                 damage = get_attack_damage(attack_type)
                 health -= damage
                 player.take_damage(damage)
+                projectile.kill()
+
+                if health <= 0:
+                    pygame.mixer.music.pause()
+                    pygame.mixer.music.load("sounds/death.mp3")
+                    pygame.mixer.music.play(-1)
+                    pygame.mixer.music.set_volume(0.2)
+                    #print("игра закончена")
+                    player_health_on_death = health
+                    player_score_on_death = score
+                    display_game_over_screen(SCREEN, font)
+                    running = False  # Прерываем игровой цикл.
+                    break  # Важно!
 
         pygame.display.flip()
         clock.tick(FPS)
+        if not running:
+            pygame.time.delay(500)
+            display_game_over_screen(SCREEN, font, "level_2.py", score)
+
+    pygame.quit()
+    sys.exit()
 
 if __name__ == "__main__":
     start_level()
